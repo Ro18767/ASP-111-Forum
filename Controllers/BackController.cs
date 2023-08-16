aee28c1e7e040d367d9cd981da15de4ebd6c2cd8
Ro18767 <timaropw@gmail.com>
2023-08-16 16:34:26 +0300

    dz 14.08.2023

---
+++ Controllers/BackController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_111.Controllers
+{
+    [Route("api/back")]
+    [ApiController]
+    public class BackController : ControllerBase
+    {
+        [HttpGet]
+        public object Get()
+        {
+            return new
+            {
+                message = "hello GET"
+            };
+        }
+
+        [HttpPost]
+        public object Post()
+        {
+            return new
+            {
+                message = "hello POST"
+            };
+        }
+
+        [HttpPut]
+        public object Put()
+        {
+            return new
+            {
+                message = "hello PUT"
+            };
+        }
+        [HttpDelete]
+        public object Delete()
+        {
+            return new
+            {
+                message = "hello DELETE"
+            };
+        }
+    }
+}
